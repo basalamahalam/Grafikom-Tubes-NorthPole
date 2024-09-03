c218aa84d1f0a22bddbebf2ee008c60d27510f22
basalamahalam <basalamahalam@upi.edu>
2024-09-03 23:01:54 +0700

    northPole

---
+++ 4.1.texture.fs
@@ -0,0 +1,12 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoord;
+in vec3 ourColor;
+
+uniform sampler2D texture1;
+
+void main()
+{
+    FragColor = texture(texture1, TexCoord)*vec4(ourColor, 0.1);
+}
